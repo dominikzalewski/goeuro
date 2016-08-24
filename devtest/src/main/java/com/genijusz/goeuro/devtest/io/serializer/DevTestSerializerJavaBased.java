0639033a5a856272dcb3d9de2f11b9cf6af81074
Dominik Zalewski <dominikz@genijusz.com>
2016-08-24 09:07:35 +0200

    final touches

--- devtest/src/main/java/com/genijusz/goeuro/devtest/io/serializer/DevTestSerializerJavaBased.java
+++ devtest/src/main/java/com/genijusz/goeuro/devtest/io/serializer/DevTestSerializerJavaBased.java
@@ -9,18 +9,19 @@ import com.genijusz.goeuro.devtest.dto.DevTestRow;
 public class DevTestSerializerJavaBased implements DevTestSerializer {
 
 	private static final String NEW_LINE = System.getProperty("line.separator");
+	private static final char CSV_SEPARATOR = ',';
 
 	@Override
 	public void serialize(List<DevTestRow> rows, Writer writer) throws IOException {
 		for (DevTestRow row : rows) {
 			writer.write(String.valueOf(row.getId()));
-			writer.write(',');
+			writer.write(CSV_SEPARATOR);
 			writer.write(row.getName());
-			writer.write(',');
+			writer.write(CSV_SEPARATOR);
 			writer.write(row.getType());
-			writer.write(',');
+			writer.write(CSV_SEPARATOR);
 			writer.write(String.valueOf(row.getLatitude()));
-			writer.write(',');
+			writer.write(CSV_SEPARATOR);
 			writer.write(String.valueOf(row.getLongitude()));
 			writer.write(NEW_LINE);
 		}
